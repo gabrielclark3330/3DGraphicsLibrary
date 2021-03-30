0419852a71858cd0cf2d843e058a716169e50d7f
Gabriel Clark <gabrielclark3330@gmail.com>
2021-03-29 23:40:47 -0500

    Added the initial demo script for displaying points to screen.

---
+++ 3DGraphicsLibrary/src/main/PointsEx.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class PointsEx extends JFrame {
+
+    public PointsEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        final Surface surface = new Surface();
+        add(surface);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Timer timer = surface.getTimer();
+                timer.stop();
+            }
+        });
+
+        setTitle("Points");
+        setSize(350, 250);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                PointsEx ex = new PointsEx();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
